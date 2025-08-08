16f5a3613d3d54aef171496ce010184354f25efe
kuresmil <kuresmil@fit.cvut.cz>
2025-08-08 16:07:09 +0200

    Fix: Config pickle can be loaded without depending on the numpy version

--- cesnet_tszoo/utils/file_utils.py
+++ cesnet_tszoo/utils/file_utils.py
@@ -1,10 +1,17 @@
 import pickle
 from typing import Any
 
+import numpy as np
+
 import yaml
 
 
 def pickle_load(path: str):
+    # Help NumPy 1.x unpickle NumPy 2.x pickles, please use this sparingly and only temporarily
+    if np.__version__[:2] == "1.":
+        import sys
+        sys.modules["numpy._core.numeric"] = np.core.numeric
+
     with open(path, "rb") as f:
         loaded_object = pickle.load(f)
     return loaded_object
